e65ec9b6c140263100b8eb6becae1a61803273f3
Patrick Lühne <patrick-github@luehne.de>
2016-11-24 03:11:36 +0100

    Fixed incorrect auxiliary variable naming.

--- include/anthem/StatementVisitor.h
+++ include/anthem/StatementVisitor.h
@@ -45,7 +45,7 @@ struct StatementVisitor
 				if (i != context.headTerms.cbegin())
 					outputStream << ", ";
 
-				const auto variableName = std::string(AuxiliaryBodyVariablePrefix) + std::to_string(i - context.headTerms.cbegin());
+				const auto variableName = std::string(AuxiliaryHeadVariablePrefix) + std::to_string(i - context.headTerms.cbegin());
 
 				outputStream
 					<< output::Variable(variableName.c_str())

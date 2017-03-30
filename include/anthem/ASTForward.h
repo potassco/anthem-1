584e631cc011a5a4a5e8b7f29f3f13fd3e71339a
Patrick Lühne <patrick-github@luehne.de>
2017-03-30 17:03:57 +0200

    Removed now unneeded declarations.

--- include/anthem/ASTForward.h
+++ include/anthem/ASTForward.h
@@ -1,9 +1,7 @@
 #ifndef __ANTHEM__AST_FORWARD_H
 #define __ANTHEM__AST_FORWARD_H
 
-#include <memory>
 #include <experimental/optional>
-#include <vector>
 
 #include <clingo.hh>
 
@@ -40,26 +38,6 @@ struct Predicate;
 struct SpecialInteger;
 struct String;
 struct Variable;
-
-using AndPointer = std::unique_ptr<And>;
-using BinaryOperationPointer = std::unique_ptr<BinaryOperation>;
-using BiconditionalPointer = std::unique_ptr<Biconditional>;
-using BooleanPointer = std::unique_ptr<Boolean>;
-using ComparisonPointer = std::unique_ptr<Comparison>;
-using ConstantPointer = std::unique_ptr<Constant>;
-using ExistsPointer = std::unique_ptr<Exists>;
-using ForAllPointer = std::unique_ptr<ForAll>;
-using FunctionPointer = std::unique_ptr<Function>;
-using ImpliesPointer = std::unique_ptr<Implies>;
-using InPointer = std::unique_ptr<In>;
-using IntegerPointer = std::unique_ptr<Integer>;
-using IntervalPointer = std::unique_ptr<Interval>;
-using NotPointer = std::unique_ptr<Not>;
-using OrPointer = std::unique_ptr<Or>;
-using PredicatePointer = std::unique_ptr<Predicate>;
-using SpecialIntegerPointer = std::unique_ptr<SpecialInteger>;
-using StringPointer = std::unique_ptr<String>;
-using VariablePointer = std::unique_ptr<Variable>;
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 // Variants

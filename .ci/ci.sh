fde2af5841c797ab16ea59e448d42360735980b4
Patrick Lühne <patrick@luehne.de>
2018-03-24 18:10:58 +0100

    Add clang to Travis configurations
    This adds the clang compiler to the tested Travis configurations.

--- .ci/ci.sh
+++ .ci/ci.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
+if [ "$1" = "gcc" ]
+then
+	cxx=g++
+	cc=gcc
+elif [ "$1" = "clang" ]
+then
+	cxx=clang++
+	cc=clang
+fi
+
 git submodule update --init --recursive
 mkdir -p build/debug
 cd build/debug
-cmake ../.. -GNinja -DANTHEM_BUILD_TESTS=ON
+cmake ../.. -GNinja -DANTHEM_BUILD_TESTS=ON -DCMAKE_CXX_COMPILER=${cxx} -DCMAKE_C_COMPILER=${cc}
 ninja anthem-app && ninja run-tests

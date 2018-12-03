1cdb256b20433f8a039b60fa2308a8eba216cdc6
Patrick Lühne <patrick@luehne.de>
2018-12-03 01:54:58 +0100

    Move comparison translation to separate header
    This moves the comparison operator translation from the header file
    responsible for translating the body to its own header file. This is to
    reuse the translation function in the upcoming direct head translation.

--- include/anthem/Body.h
+++ include/anthem/Body.h
@@ -4,6 +4,7 @@
 #include <algorithm>
 
 #include <anthem/AST.h>
+#include <anthem/ComparisonOperator.h>
 #include <anthem/Term.h>
 #include <anthem/Utils.h>
 #include <anthem/output/Formatting.h>
@@ -15,29 +16,6 @@ namespace anthem
 //
 // Body
 //
-////////////////////////////////////////////////////////////////////////////////////////////////////
-
-ast::Comparison::Operator translate(Clingo::AST::ComparisonOperator comparisonOperator)
-{
-	switch (comparisonOperator)
-	{
-		case Clingo::AST::ComparisonOperator::GreaterThan:
-			return ast::Comparison::Operator::GreaterThan;
-		case Clingo::AST::ComparisonOperator::LessThan:
-			return ast::Comparison::Operator::LessThan;
-		case Clingo::AST::ComparisonOperator::LessEqual:
-			return ast::Comparison::Operator::LessEqual;
-		case Clingo::AST::ComparisonOperator::GreaterEqual:
-			return ast::Comparison::Operator::GreaterEqual;
-		case Clingo::AST::ComparisonOperator::NotEqual:
-			return ast::Comparison::Operator::NotEqual;
-		case Clingo::AST::ComparisonOperator::Equal:
-			return ast::Comparison::Operator::Equal;
-	}
-
-	return ast::Comparison::Operator::NotEqual;
-}
-
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
 struct BodyTermTranslateVisitor

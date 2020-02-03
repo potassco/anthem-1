ad3a2d18f8c635d1e23957ced029adfb51cbba96
Patrick Lühne <patrick@luehne.de>
2020-02-03 22:20:13 +0100

    Add subcommand for verifying programs

--- src/main.rs
+++ src/main.rs
@@ -2,21 +2,31 @@ use structopt::StructOpt as _;
 
 #[derive(Debug, structopt::StructOpt)]
 #[structopt(name = "anthem", about = "Use first-order theorem provers with answer set programs.")]
-struct Options
+enum Command
 {
-	#[structopt(parse(from_os_str))]
-	input: Vec<std::path::PathBuf>,
+	#[structopt(about = "Verifies a logic program against a specification")]
+	VerifyProgram
+	{
+		#[structopt(parse(from_os_str))]
+		input: Vec<std::path::PathBuf>,
+	}
 }
 
 fn main()
 {
 	pretty_env_logger::init();
 
-	let options = Options::from_args();
+	let command = Command::from_args();
 
-	if let Err(error) = anthem::translate::verify_properties::translate(&options.input)
+	match command
 	{
-		log::error!("could not translate input program: {}", error);
-		std::process::exit(1)
+		Command::VerifyProgram{input} =>
+		{
+			if let Err(error) = anthem::translate::verify_properties::translate(&input)
+			{
+				log::error!("could not translate input program: {}", error);
+				std::process::exit(1)
+			}
+		},
 	}
 }

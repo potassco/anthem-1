82b3176a403cf18493baf9b1c52e18d048fbcc51
Patrick Lühne <patrick@luehne.de>
2020-02-03 22:50:33 +0100

    Require at least one input file

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ enum Command
 	#[structopt(about = "Verifies a logic program against a specification")]
 	VerifyProgram
 	{
-		#[structopt(parse(from_os_str))]
+		#[structopt(parse(from_os_str), required(true))]
 		input: Vec<std::path::PathBuf>,
 	}
 }
